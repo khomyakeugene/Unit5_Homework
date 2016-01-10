c80a81dfa3ed87dcccee2c0cc5e1b2247a348841
Olga Kulikova <masyakot@gmail.com>
2016-01-11 00:53:29 +0200

    added comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ import com.company.tests.TestArrayInvestigation;
 import com.company.tests.TestMatrixInvestigation;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { //желательно не выпускать эксепшн из программы, а ловить и обрабатывать его,
+        //чтобы программа завершалась без ошибок.
         new TestArrayInvestigation().demonstrateArrayInvestigation();
 
         // Additional task of Unit 5
