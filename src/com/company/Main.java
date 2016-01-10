ef9bb8fdff2963c6eb9fce27422b755b655ed655
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-01-11 01:05:11 +0200

    additional task of Unit 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import com.company.tests.TestMatrixInvestigation;
 
 public class Main {
     public static void main(String[] args) {
+        // Main task of Unit 5
         new TestArrayInvestigation().demonstrateArrayInvestigation();
 
         // Additional task of Unit 5
