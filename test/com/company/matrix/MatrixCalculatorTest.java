41a049ae35bddf78b838dff41a0be29f9357003f
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-02-03 07:51:29 +0200

    Unit 11 Homework

---
+++ test/com/company/matrix/MatrixCalculatorTest.java
@@ -0,0 +1,59 @@
+package com.company.matrix;
+
+import org.junit.Test;
+
+/**
+ * Created by Yevgen on 02.02.2016 as a part of the project "Unit5_Homework".
+ */
+public class MatrixCalculatorTest {
+
+    @Test
+    public void testCheckMatrixSizeEquality() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMatrixHeight() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMatrixWidth() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckMatrixMultiplicationPossibility() throws Exception {
+
+    }
+
+    @Test
+    public void testRound() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testSub() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiplyByNumber() throws Exception {
+
+    }
+
+    @Test
+    public void testMul() throws Exception {
+
+    }
+
+    @Test
+    public void testTranspose() throws Exception {
+
+    }
+}
